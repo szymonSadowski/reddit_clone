842f194b74205497e038ab736461ece4177e6767
szymonSadowski <szymon.ssadows@gmail.com>
2020-12-14 21:29:28 +0100

    Added apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
-import { Post } from './entities/Post';
+import { HelloResolver } from './resolvers/hello';
+// import { Post } from './entities/Post';
 import { __prod__ } from './constants';
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post);
+    const app = express();
 
-    /**
-     * * shows post
-         const posts = await orm.em.find(Post, {}); 
-         console.log(posts)
-     */
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        }),
+    });
 
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
 
 };
 
