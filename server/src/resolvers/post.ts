bba10e74422ee8e4ee8f3bf16f0757528c09d68c
szymonSadowski <szymon.ssadows@gmail.com>
2021-02-10 21:00:47 +0100

    add docker file

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -166,12 +166,15 @@ export class PostResolver {
     @Ctx() { req }: MyContext
   ): Promise<Post | null> {
     const result = await getConnection()
-    .createQueryBuilder()
-    .update(Post)
-    .set({title, text})
-    .where('id = :id and "creatorId" = :creatorId', { id, creatorId: req.session.userId )
-    .returning("*")
-    .execute();
+      .createQueryBuilder()
+      .update(Post)
+      .set({ title, text })
+      .where('id = :id and "creatorId" = :creatorId', {
+        id,
+        creatorId: req.session.userId,
+      })
+      .returning("*")
+      .execute();
     return result.raw[0];
   }
 
